6d7c40a2fe87c914207802a5e2b5f6925c25676a
NaziraMurataeva <nazira1988.nm@gmail.com>
2021-12-04 00:28:35 +0600

    urok 36 na uroke

---
+++ app/src/main/java/com/example/urok36/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.urok36
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupFragment()
+    }
+
+    private fun setupFragment() {
+       val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,FragmentA())
+
+        transaction.commit()
+    }
+}
